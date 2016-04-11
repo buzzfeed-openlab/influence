b6a00fda2873e248c3bd393c1035a81fdaa93760
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-04-11 15:56:45 -0700

    navigation!

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -4,6 +4,8 @@ import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 import Layout from "./layout";
 import TeamPage from "./team-page";
+import FeedPage from "./feed-page";
+import UpcomingPage from "./upcoming-page";
 
 const app = document.getElementById('app');
 
@@ -12,6 +14,8 @@ ReactDOM.render(
     <Route path="/" component={Layout}>
       
       <IndexRoute component={TeamPage}></IndexRoute>
+      <Route path="feed" name="feed" component={FeedPage}></Route>
+      <Route path="upcoming" name="upcoming" component={UpcomingPage}></Route>
 
     </Route>
   </Router>,
---
+++ web/public/js/upcoming-page.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class TeamPage extends React.Component {
+  render() {
+    console.log('upcoming page!');
+
+    return (
+      <div>
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="well text-center">
+              {'UPCOMING PAGE!!!'}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
