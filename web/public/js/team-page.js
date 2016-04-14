be50f969e93d9606aad93704f39862ae52f81ff7
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-04-13 17:00:24 -0700

    access interests through props

--- web/public/js/team-page.js
+++ web/public/js/team-page.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { VirtualScroll } from 'react-virtualized';
 
-const industries = ["Health Services/HMOs","Forestry & Forest Products","TV/Movies/Music","Air Transport","Insurance","Electric Utilities","Misc Manufacturing & Distributing","Republican/Conservative","Pharmaceuticals/Health Products","Computers/Internet","Education","Savings & Loans","Lodging/Tourism","Misc Services","Foreign & Defense Policy","Joint Candidate Cmte","Human Rights","Health Professionals","Hospitals/Nursing Homes","Mining","Sea Transport","Securities & Investment","Candidate Committees","Business Services","Misc Business","Accountants","Misc Defense","Building Materials & Equipment","Non-contribution","Employer Listed/Category Unknown","Party Committees","Environment","Misc Issues","Public Sector Unions","Food & Beverage","Misc Finance","Non-Profit Institutions","Special Trade Contractors","Industrial Unions","Misc Unions","Automotive","Agricultural Services/Products","Construction Services","Business Associations","Waste Management","Printing & Publishing","Beer, Wine & Liquor","Leadership PACs","Civil Servants/Public Officials","Pro-Israel","Crop Production & Basic Processing","Real Estate","Retail Sales","Misc Health","Telecom Services & Equipment","Railroads","Other","Defense Aerospace","No Employer Listed or Found","Intl Orgs","Misc Energy","General Contractors","Democratic/Liberal","Abortion Policy/Pro-Life","Livestock","Chemical & Related Manufacturing","Lobbyists","Home Builders","Generic Occupation/Category Unknown","Finance/Credit Companies","Defense Electronics","Trucking","Homemakers/Non-income earners","Commercial Banks","Women's Issues","Transportation Unions","Oil & Gas","Environmental Svcs/Equipment","Food Processing & Sales","Misc Transport","Gun Rights","Lawyers/Law Firms","Fisheries & Wildlife","Retired","Abortion Policy/Pro-Choice","Telephone Utilities","Electronics Mfg & Services","Misc Agriculture","Recreation/Live Entertainment","Steel Production","Building Trade Unions","Tobacco","Clergy & Religious Organizations","Textiles","Gun Control","Unknown","Credit Unions","Casinos/Gambling","Dairy","Poultry & Eggs","Party Committee Transfer","Candidate Self-finance","Misc Communications/Electronics"];
-
 export default class TeamPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            interests: ["Health Services/HMOs","Forestry & Forest Products","TV/Movies/Music","Air Transport","Insurance","Electric Utilities","Misc Manufacturing & Distributing","Republican/Conservative","Pharmaceuticals/Health Products","Computers/Internet","Education","Savings & Loans","Lodging/Tourism","Misc Services","Foreign & Defense Policy","Joint Candidate Cmte","Human Rights","Health Professionals","Hospitals/Nursing Homes","Mining","Sea Transport","Securities & Investment","Candidate Committees","Business Services","Misc Business","Accountants","Misc Defense","Building Materials & Equipment","Non-contribution","Employer Listed/Category Unknown","Party Committees","Environment","Misc Issues","Public Sector Unions","Food & Beverage","Misc Finance","Non-Profit Institutions","Special Trade Contractors","Industrial Unions","Misc Unions","Automotive","Agricultural Services/Products","Construction Services","Business Associations","Waste Management","Printing & Publishing","Beer, Wine & Liquor","Leadership PACs","Civil Servants/Public Officials","Pro-Israel","Crop Production & Basic Processing","Real Estate","Retail Sales","Misc Health","Telecom Services & Equipment","Railroads","Other","Defense Aerospace","No Employer Listed or Found","Intl Orgs","Misc Energy","General Contractors","Democratic/Liberal","Abortion Policy/Pro-Life","Livestock","Chemical & Related Manufacturing","Lobbyists","Home Builders","Generic Occupation/Category Unknown","Finance/Credit Companies","Defense Electronics","Trucking","Homemakers/Non-income earners","Commercial Banks","Women's Issues","Transportation Unions","Oil & Gas","Environmental Svcs/Equipment","Food Processing & Sales","Misc Transport","Gun Rights","Lawyers/Law Firms","Fisheries & Wildlife","Retired","Abortion Policy/Pro-Choice","Telephone Utilities","Electronics Mfg & Services","Misc Agriculture","Recreation/Live Entertainment","Steel Production","Building Trade Unions","Tobacco","Clergy & Religious Organizations","Textiles","Gun Control","Unknown","Credit Unions","Casinos/Gambling","Dairy","Poultry & Eggs","Party Committee Transfer","Candidate Self-finance","Misc Communications/Electronics"]
         };
     }
 
     render() {
         console.log('team page!');
 
-        return this.state.team ? <TeamViewer/> : <TeamBuilder/>;
+        return this.state.team ? <TeamViewer /> : <TeamBuilder interests={this.state.interests} />;
     }
 }
 
-class TeamRow extends React.Component {
+class InterestRowDetailed extends React.Component {
     render() {
         return (
             <div>
-                {this.props.industry}
+                {this.props.interest}
             </div>
         );
     }
@@ -37,10 +36,10 @@ class TeamBuilder extends React.Component {
                         <VirtualScroll
                             width={300}
                             height={300}
-                            rowsCount={industries.length}
+                            rowsCount={this.props.interests.length}
                             rowHeight={20}
                             rowRenderer={
-                              index => <TeamRow key={index} industry={industries[index]} />
+                              i => <InterestRowDetailed interest={this.props.interests[i]} />
                             }
                         />
                     </div>
